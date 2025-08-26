f54b693011f3c53cd92e7fcce148414e4c8635e3
pedokylee <kyleelle17@gmail.com>
2025-08-26 10:29:19 +0800

    Update Greeting.vue

--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -1,27 +1,30 @@
 <template>
-  <p>{{ capitalizedMessage }}</p>
+  <section>
+    <h2>Greeting Component</h2>
+
+    <p>{{ capitalizedMessage }}</p>
+
+    <p>Hello, {{ username }}</p>
+  </section>
 </template>
 
 <script>
-import { capitalize } from '../utils.js';
+import { capitalize } from "../utils.js";
 
 export default {
-  name: 'UserGreeting',
+  name: "GreetingComponent",
   props: {
-    username: {
-      type: String,
-      required: true
-    }
+    username: { type: String, required: true },
   },
   data() {
     return {
-      message: `hello, ${this.username}! welcome to the My Vue App.`
-    }
+      message: "hello from greeting component",
+    };
   },
   computed: {
     capitalizedMessage() {
-      return this.message.split(' ').map(word => capitalize(word)).join(' ');
-    }
-  }
-}
+      return capitalize(this.message);
+    },
+  },
+};
 </script>
